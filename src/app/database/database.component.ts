517e59f66543b60e21b9a0e3a396121d7175d498
MaconHolliman <macon.holliman@usm.edu>
2017-11-21 19:09:35 -0800

    Moved all of the test functions to the database component and wrote a few new ones. Made them all work with our current API. In addition, I added a get all button which pulls every table from our database and stores it into the variable that is equal to the plural form for the url. e.g. job database stored in jobs variable. Got rid of a lot of the cluttered code, left all of that example code in the create poll component those if someone wants to reference it still. Removed the createpoll component from being displayed in the main html file.

--- src/app/database/database.component.ts
+++ src/app/database/database.component.ts
@@ -11,7 +11,87 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DatabaseComponent implements OnInit {
 
-  constructor() { }
+  
+  results: string;
+  body: string;
+  
+  
+  
+  id: number = 1;
+
+  //getAll() stores the full tables in these variables when called.
+  Employees: string;
+  Accesses: string;
+  Assigned_tos: string;
+  Doings: string;
+  Items: string;
+  Items_stored: string;
+  Jobs: string;
+  Take_froms: string;
+  Vehicles: string;
+  Warehouses: string;
+  
+  
+
+  
+  constructor(private http: HttpClient) {}
+
+  onSubmit(f: NgForm) {
+   console.log(f.value);  // { first: '', last: '' }
+   this.body = f.value;
+   console.log(this.body);
+    console.log(f.valid);  // false
+  }
+  
+
+  
+  getEmployees(): void {
+    this.http.get('http://localhost:3000/api/Employees').subscribe(data => {
+      this.results = JSON.stringify(data);
+      console.log(this.results);
+    });
+  }
+
+
+  getEmployeeById(): void {
+    this.http.get('http://localhost:3000/api/Employees/' + this.id).subscribe(data => {
+      this.results = JSON.stringify(data);
+      console.log(this.results);
+    });
+  }
+
+  postEmployee(): void {
+    this.http.post('http://localhost:3000/api/Employees', this.body).subscribe(data => {
+      this.results = JSON.stringify(data);
+      console.log(this.results);
+    });
+  }
+
+
+
+  getAllAction(table: string): void {
+this.http.get('http://localhost:3000/api/' + table).subscribe(data => {
+      this[table] = JSON.stringify(data);
+    });
+  }
+  
+  getAll(){
+   this.getAllAction('Employees');
+   this.getAllAction('Accesses');
+   this.getAllAction('Assigned_tos');
+   this.getAllAction('Doings');
+   this.getAllAction('Items');
+   this.getAllAction('Items_stored');
+   this.getAllAction('Jobs');
+   this.getAllAction('Take_froms');
+   this.getAllAction('Vehicles');
+   this.getAllAction('Warehouses');
+   
+  
+  }
+
+ 
+
 
   ngOnInit() {
   }
